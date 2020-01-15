80ce8edde3841dc4d0cac13279b32079139fc8ea
John Batty <login@johnbatty.me>
2020-01-15 04:58:42 -0700

    added dropdown to nav bar

--- web/nav.php
+++ web/nav.php
@@ -3,9 +3,22 @@
                 <div class="navbar-header">
                     <div class="navbar-brand" href="#">John Batty's Homepage</div>
                 </div>
-                <ul class="nav navbar-nav">
-                    <li><a href="/assignments">Assignments Page</a></li>
-                
+                <button class="btn btn-primary dropdown-toggle">Assignments
+                        <span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Week 1</a></li>
+                    <li><a href="#">Week 2</a></li>
+                    <li><a href="#">Week 3</a></li>
+                    <li><a href="#">Week 4</a></li>
+                    <li><a href="#">Week 5</a></li>
+                    <li><a href="#">Week 6</a></li>
+                    <li><a href="#">Week 7</a></li>
+                    <li><a href="#">Week 8</a></li>
+                    <li><a href="#">Week 9</a></li>
+                    <li><a href="#">Week 10</a></li>
+                    <li><a href="#">Week 11</a></li>
+                    <li><a href="#">Week 12</a></li>
+                    <li><a href="#">Week 13</a></li>
                 </ul>
             </div>
         </nav>
