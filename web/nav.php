5fcbfa4bf42f522ca53929733b7ddcde056b91f0
John Batty <login@johnbatty.me>
2020-01-15 05:05:25 -0700

    2nd dropdown

--- web/nav.php
+++ web/nav.php
@@ -4,7 +4,7 @@
                     <div class="navbar-brand" href="#">John Batty's Homepage</div>
                 </div>
                 <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Assignments
+                    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Teach Activities
                         <span class="caret"></span></button>
                     <ul class="dropdown-menu">
                         <li><a href="#">Week 1</a></li>
@@ -21,6 +21,25 @@
                         <li><a href="#">Week 12</a></li>
                         <li><a href="#">Week 13</a></li>
                     </ul>
-                    </div>
+                </div>
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Ponder Assignments
+                        <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Week 1</a></li>
+                        <li><a href="#">Week 2</a></li>
+                        <li><a href="#">Week 3</a></li>
+                        <li><a href="#">Week 4</a></li>
+                        <li><a href="#">Week 5</a></li>
+                        <li><a href="#">Week 6</a></li>
+                        <li><a href="#">Week 7</a></li>
+                        <li><a href="#">Week 8</a></li>
+                        <li><a href="#">Week 9</a></li>
+                        <li><a href="#">Week 10</a></li>
+                        <li><a href="#">Week 11</a></li>
+                        <li><a href="#">Week 12</a></li>
+                        <li><a href="#">Week 13</a></li>
+                    </ul>
+                </div>
             </div>
         </nav>
