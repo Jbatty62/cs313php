467c649756f96381363420cfe429b44b7349c825
batty-john <login@johnbatty.me>
2020-01-27 08:39:47 -0700

    updated nav bar

--- web/nav.php
+++ web/nav.php
@@ -1,44 +1,41 @@
 <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <div class="navbar-brand" href="#">John Batty's Homepage</div>
+                    <img src="logo.png">
                 </div>
                 <div class="dropdown">
                     <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Teach Activities
                         <span class="caret"></span></button>
                     <ul class="dropdown-menu">
-                        <li><a href="#">Week 1</a></li>
-                        <li><a href="#">Week 2</a></li>
-                        <li><a href="#">Week 3</a></li>
-                        <li><a href="#">Week 4</a></li>
-                        <li><a href="#">Week 5</a></li>
-                        <li><a href="#">Week 6</a></li>
-                        <li><a href="#">Week 7</a></li>
-                        <li><a href="#">Week 8</a></li>
-                        <li><a href="#">Week 9</a></li>
-                        <li><a href="#">Week 10</a></li>
-                        <li><a href="#">Week 11</a></li>
-                        <li><a href="#">Week 12</a></li>
-                        <li><a href="#">Week 13</a></li>
+                        <li><a href="/teach02/">Week 2</a></li>
+                        <li><a href="/teach03/">Week 3</a></li>
+                        <li><a href="/teach04/">Week 4</a></li>
+                        <li><a href="/teach05/">Week 5</a></li>
+                        <li><a href="/teach06/">Week 6</a></li>
+                        <li><a href="/teach07/">Week 7</a></li>
+                        <li><a href="/teach08/">Week 8</a></li>
+                        <li><a href="/teach09/">Week 9</a></li>
+                        <li><a href="/teach10/">Week 10</a></li>
+                        <li><a href="/teach11/">Week 11</a></li>
+                        <li><a href="/teach12/">Week 12</a></li>
+                        <li><a href="/teach13/">Week 13</a></li>
                     </ul>
                 </div>
                 <div class="dropdown">
                     <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Ponder Assignments
                         <span class="caret"></span></button>
                     <ul class="dropdown-menu">
-                        <li><a href="#">Week 1</a></li>
-                        <li><a href="#">Week 2</a></li>
-                        <li><a href="#">Week 3</a></li>
-                        <li><a href="#">Week 4</a></li>
-                        <li><a href="#">Week 5</a></li>
-                        <li><a href="#">Week 6</a></li>
-                        <li><a href="#">Week 7</a></li>
-                        <li><a href="#">Week 8</a></li>
-                        <li><a href="#">Week 9</a></li>
-                        <li><a href="#">Week 10</a></li>
-                        <li><a href="#">Week 11</a></li>
-                        <li><a href="#">Week 12</a></li>
-                        <li><a href="#">Week 13</a></li>
+                        <li><a href="/ponder03/">Week 3</a></li>
+                        <li><a href="/ponder04/">Week 4</a></li>
+                        <li><a href="/ponder05/">Week 5</a></li>
+                        <li><a href="/ponder06/">Week 6</a></li>
+                        <li><a href="/ponder07/">Week 7</a></li>
+                        <li><a href="/ponder08/">Week 8</a></li>
+                        <li><a href="/ponder09/">Week 9</a></li>
+                        <li><a href="/ponder10/">Week 10</a></li>
+                        <li><a href="/ponder11/">Week 11</a></li>
+                        <li><a href="/ponder12/">Week 12</a></li>
+                        <li><a href="/ponder13/">Week 13</a></li>
                     </ul>
                 </div>
             </div>
