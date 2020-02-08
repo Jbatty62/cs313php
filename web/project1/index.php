33c49e09fb6b75c24abeb48e8e25a03326fbb627
Jbatty62 <login@johnbatty.me>
2020-02-08 13:47:35 -0700

    added links from index.php

---
+++ web/project1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>The Last Stand RPG</title>
+</head>
+<body>
+    
+    <a href="races.php">Races</a>
+    <a href="classess.php">Classes</a>
+    <a href="items.php">Items</a>
+    <a href="spells.php">Spells</a>
+    <a href="account.php">Account</a>
+    <a href="character.php">Character</a>
+    <a href="abilities.php">Abilities</a>
+    <a href="/shop">Shop</a>
+    
+    
+</body>
+</html>
