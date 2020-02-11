4a44dbdb01632233176d7302bba573fe17ba9277
Jbatty62 <login@johnbatty.me>
2020-02-11 02:29:35 -0700

    initial database connect attempt

---
+++ web/project1/races.php
@@ -0,0 +1,47 @@
+<?php
+
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Races</title>
+</head>
+<body>
+    <div id="container">
+        <main id="main">
+            <?php
+                foreach ($db->query('SELECT * FROM races') as $row) {                
+                    echo '<div class="card">'. $row['name'] . '</div>';
+                }
+            ?>
+        </main>
+
+    </div>
+    
+</body>
+</html>
