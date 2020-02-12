9f32b58664ad618926a9eeaca6d15b4ac51b7815
Jbatty62 <login@johnbatty.me>
2020-02-12 02:49:27 -0700

    added includes

--- web/index.php
+++ web/index.php
@@ -8,15 +8,7 @@
     <title>The Last Stand RPG</title>
 </head>
 <body>
-    
-    <a href="races.php">Races</a>
-    <a href="classess.php">Classes</a>
-    <a href="items.php">Items</a>
-    <a href="spells.php">Spells</a>
-    <a href="account.php">Account</a>
-    <a href="character.php">Character</a>
-    <a href="abilities.php">Abilities</a>
-    <a href="/shop">Shop</a>
+    <?php include 'nav.php'; ?>
     
     
 </body>
