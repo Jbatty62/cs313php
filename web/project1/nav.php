6b94289570869026c09ebd50d8eb30371cf06999
Jbatty62 <login@johnbatty.me>
2020-02-12 02:35:57 -0700

    Replaced Nav Bar

--- web/project1/nav.php
+++ web/project1/nav.php
@@ -1,6 +1,15 @@
-<ul class="sidenav">
-  <li><a class="active" href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
+<ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="shop.php">Shop</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Rules and Content</a>
+    <div class="dropdown-content">
+      <a href="/abilities">Abilities</a>
+      <a href="/classes">Classes</a>
+      <a href="/items">Items</a>
+      <a href="/races">Races</a>
+      <a href="/spells">Spells</a>
+    </div>
+  </li>
+  <li><a href="account.php">Account</a></li>
 </ul>
