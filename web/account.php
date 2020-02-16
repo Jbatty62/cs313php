76f5ca6f950d44679f528c599135850b405ad6ae
Jbatty62 <login@johnbatty.me>
2020-02-15 20:52:23 -0700

    testing form styles

--- web/account.php
+++ web/account.php
@@ -9,12 +9,80 @@ include 'connect.php'
         <link rel="stylesheet" type="text/css" href="styles.css">
         <script type="text/jscript" src="functions.js"></script>
         <style> 
-    
+        
+/***************************
+* Form Styles --->
+***************************/
+
+
+.form-group {
+  margin: 30px auto;
+  width: 450px;
+}
+
+#street {
+  width: calc(100% - 10px);
+}
+
+#city {
+  width: 50%;
+}
+
+#state {
+  width: 15%;
+}
+
+#zip {
+  width: calc(100% - 50% - 15% - 31px);
+}
+
+#county {
+  width: 45%;
+}
+
+#country {
+  width: calc(100% - 45% - 20px);
+}
+
+#firstname {
+  width: calc(50% - 10px);
+}
+
+#lastname {
+  width: calc(50% - 10px);
+}
+
+input {
+    margin: 5px 5px;
+    padding: 6px 12px;
+    font-size: 14px;
+    line-height: 1.5;
+    color: #555;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-shadow: inset 0 1px 1px rgba(0,0,0,.25);
+    transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
+    float: left;
+}
+
+#submit {
+  border-radius: 4px;
+  background-color: var(--color-2);
+  border: none;
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 18px;
+  padding: 8px;
+  transition: all 0.5s;
+  cursor: pointer;
+  float:right;
+}
     
         </style>
     </head>
     <body>
-    <?php include '../nav.php';  ?>
+    <?php include '/nav.php';  ?>
         <div id=container>
         
             <main id="main">
