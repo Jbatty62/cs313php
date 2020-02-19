1b4153335787317aba0135281093f636abf980d3
Jbatty62 <login@johnbatty.me>
2020-02-18 17:37:05 -0700

    Added code commenting to diceRoller

--- web/diceroller/functions.js
+++ web/diceroller/functions.js
@@ -1,47 +1,118 @@
 
+//Setup the initial varriables for the DiceRoller App
 {
 let dice = [new Dice(3, 0), new Dice(3, 0),new Dice(3,0)],
         diceSet = new DiceSet(dice, "Dice Set 1"),
         diceSets = [diceSet];
+
+        var board = new Board(diceSets);
         
-var board = new Board(diceSets);
 }
 
+
+ /*****************************************
+ * removeDice()
+ * Remove a Dice from a DiceSet.
+ *****************************************/
 function removeDice(DiceSetId) {
    board.removeDice(DiceSetId);
 }
 
+ /*****************************************
+ * addDice()
+ * Add a Dice to a DiceSet.
+ *****************************************/
 function addDice(diceSetID) {
    board.addDice(diceSetID);
 }
 
+ /*****************************************
+ * rollAll()
+ * Roll all of the dice in a DiceSet.
+ *****************************************/
 function rollAll(diceSetID) {
    board.rollAll(diceSetID);
  }
 
-/*
-function removeDiceSet(ID) {
+ /*****************************************
+ * removeDiceSet()
+ * Remove a DiceSet from the board.
+ *****************************************/
+function removeDiceSet(diceSetID) {
+    board.removeDiceSet(diceSetID);
+}
+
+/*****************************************
+ * addDiceSet()
+ * create a new DiceSet and add it to the board.
+ *****************************************/
+function addDiceSet() {
+    let dice = [new Dice(3, 0), new Dice(3, 0),new Dice(3,0)],
+        diceSet = new DiceSet(dice, "Dice Set 1");
+    board.addDiceSet(diceSet);
+}
+
+
+/*****************************************
+ * saveBoard()
+ * Store a board in Local Storage
+ *****************************************/
+function saveBoard() {
+    window.localStorage.setItem("board", board.toJSON());
+}
+
+/*****************************************
+ * loadBoard()
+ * Load a board from a JSON string
+ *****************************************/
+function loadBoard () {
+
+    //If Local Storage is already Set
+    if (window.localStorage.getItem("board")) {
+        var object = JSON.parse(window.localStorage.getItem("board")); //Recover stored JSON into object
     
-    document.getElementById(ID).remove();
+        
+        diceSets = []; //Array to store all DiceSets
+        for (let i = 0; i< object.length; i++) {
+            var dice = []; //Array to hold dice for one DiceSet
+            for(let j = 0; j < object[i].length; j++) {
+                dice.push(new Dice(object[i][j].numSides, object[i][j].minimum));
+                
+            }
+            diceSets.push(new DiceSet(dice)); //Add array of dice to a new DiceSet and push to array of DiceSets
+        }
+        //Create new board and replace the old one
+        board = new Board(diceSets);
+    }
+    
+    board.display();
 }
 
 
 
-function getNumDice(diceSetI) {
-    return 6;
-}
-*/
 
 
 
 
 
-
-
-
-
-
-
+/**************************************************************************
+*
+*   Dice Object 
+*   An object representing a single dice. Typically inside of a diceSet.
+*
+*   - Variables
+*       Minimum: Typically 1 or 0. Raises the minimum possible value of roll() by this amount
+*       numSides: The span of the range of possible values. Possible values span minimum to minimum + numSides.
+*       id:        The id of the dom element that represtens this object
+*       value:    The current value of the dice object. The last rolled number.
+*       dom:        The contents of the HTML for the associated DOM element.
+*
+*   - Methods     
+*       setValue(): A basic setter function for the value member variable that also adjusts the image displayed on the dom.
+*       roll():     Get a random number within the range and call setValue()
+*       createDOM(): Generate the HTML to be inserted into the DOM.
+*       toJSON():   Generates json that can easily be used to reload the board or element.
+**************************************************************************/
 function Dice(numSides, minimum) {
     this.minimum = minimum;
     this.numSides = numSides;
@@ -58,9 +129,9 @@ function Dice(numSides, minimum) {
     };
     this.roll = function () {
         var roll = Math.floor(Math.random() * numSides);
-        if (minimum === 1) {
-            roll += 1;
-        }
+        
+            roll += minimum;
+        
         this.setValue(roll);
         return roll;
     };
@@ -74,8 +145,31 @@ function Dice(numSides, minimum) {
 
     }
     this.dom = null;    
+    this.toJSON = function () {
+        return '{"numSides": "' + this.numSides + '", "minimum": "' + this.minimum + '"}';
+    }
 } 
 
+
+
+
+/**************************************************************************
+*
+*   DiceSet Object 
+*   An object representing a sset of dice. Used by and stored within the Board object.
+*
+*   - Variables
+*       Name: The title of the DiceSet. Currently unused, in a future update, this will display on the DiceSet.
+*       dice: The array that holds all of the Dice objects stored in the DiceSet
+*       id:        The id of the dom element that represtens this object
+*       dom:        The contents of the HTML for the associated DOM element.
+*
+*   - Methods     
+*       setValue(): A basic setter function for the value member variable that also adjusts the image displayed on the dom.
+*       roll():     Get a random number within the range and call setValue()
+*       createDOM(): Generate the HTML to be inserted into the DOM.
+*       toJSON():   Generates json that can easily be used to reload the board or element.
+**************************************************************************/
 function DiceSet(dice, name) {
     this.name = name;
     this.dice = dice;
@@ -108,20 +202,22 @@ function DiceSet(dice, name) {
         dom.setAttribute("id",this.id);
         dom.setAttribute("class","diceset");
 
+        var inputs = document.createElement("div");
+        inputs.setAttribute("class","inputs");
 
         var minusButton = document.createElement("span");
-        minusButton.setAttribute("class", "minus");
+        //minusButton.setAttribute("class", "minus");
         minusButton.setAttribute("onclick", "removeDice(" + this.id + ")")
         minusButton.innerHTML = "-";
         
 
         var plusButton = document.createElement("span");
-        plusButton.setAttribute("class", "plus");
+        //plusButton.setAttribute("class", "plus");
         plusButton.innerHTML = "+";
         plusButton.setAttribute("onclick", "addDice(" + this.id + ")")
 
         var rollAllButton = document.createElement("span");
-        rollAllButton.setAttribute("class", "plus");
+        //rollAllButton.setAttribute("class", "plus");
         rollAllButton.innerHTML = "Roll";
         rollAllButton.setAttribute("onclick", "rollAll(" + this.id + ")")
 
@@ -136,11 +232,12 @@ function DiceSet(dice, name) {
 
         var xButton = document.createElement("span");
         xButton.setAttribute("class", "close");
-        //xbutton.addEventListener("click", removeDiceSet(this.id))
+        xButton.setAttribute("onclick", "removeDiceSet(" + this.id + ")");
 
-        dom.appendChild(minusButton);
-        dom.appendChild(plusButton);
-        dom.appendChild(rollAllButton);
+        inputs.appendChild(minusButton);
+        inputs.appendChild(plusButton);
+        inputs.appendChild(rollAllButton);
+        dom.appendChild(inputs);
         dom.appendChild(diceContainer);
         dom.appendChild(xButton);
 
@@ -149,9 +246,40 @@ function DiceSet(dice, name) {
 
     };
     this.dom = null;
+    this.toJSON = function() {
+        var json = '[';
+        for (let i = 0; i < dice.length; i++) {
+            json += dice[i].toJSON();
+            if (i != dice.length - 1) {
+                json += ",";
+            }
+        }
+
+        json += "]";
+        return json;
+    }
+
 
 }
 
+/**************************************************************************
+*
+*   Board Object 
+*   An object representing the entire space of the application.
+*
+*   - Variables
+*       diceSets: The array that holds all of the DiceSet objects stored in the Board
+*    
+*
+*   - Methods     
+*       display(): Updates and inserts the HTML into the DOM.
+*       removeDice():   Locates the DiceSet indicated by the ID of the clicked element and calls that object's removeDice() method.
+*       addDice():   Locates the DiceSet indicated by the ID of the clicked element and calls that object's addDice() method.
+*       rollAll():   Locates the DiceSet indicated by the ID of the clicked element and calls that object's rollAll() method.
+*       removeDiceSet():   Locates the DiceSet indicated by the ID of the clicked element removes it from the board.
+*       addDiceSet():   creates a new diceSet, adds it to the board, and used display() to regenerate the HTML on the page.
+*       toJSON():   Generates json that can easily be used to reload the board.
+**************************************************************************/
 
 function Board (diceSets) {
     this.diceSets = diceSets;
@@ -163,9 +291,25 @@ function Board (diceSets) {
                 diceSets[i].createDOM();
                 document.getElementById("main").appendChild(diceSets[i].dom);
             
+                
 
             }
-          
+            let button = document.createElement("button");
+            button.setAttribute("onclick", "addDiceSet()");
+            button.innerHTML = "Add Dice Set"
+
+            document.getElementById("main").appendChild(button);
+
+            button = document.createElement("button");
+            button.setAttribute("onclick", "saveBoard()");
+            button.innerHTML = "Save Dice Sets";
+            document.getElementById("main").appendChild(button);
+
+            button = document.createElement("button");
+            button.setAttribute("onclick", "loadBoard()");
+            button.innerHTML = "Load Dice Sets";
+            document.getElementById("main").appendChild(button);
+
 
     };
     this.removeDice = function (diceSetID) {
@@ -193,10 +337,44 @@ function Board (diceSets) {
             }
         } 
     };
+
+    this.removeDiceSet = function (diceSetID) {
+        console.log("in board remove dice set");
+        
+        for (let i = 0; i < diceSets.length; i++) {
+            if (diceSets[i].id === diceSetID) {
+                this.diceSets.splice(i,1);
+                break;
+            }
+
+        } 
+        this.display();
+    };
+
+    this.addDiceSet = function (diceSet) {
+        
+        this.diceSets.push(diceSet);
+        this.display();
+    };
+    this.toJSON = function () {
+        let json = '[';
+        for (let i = 0; i < diceSets.length; i++) {
+            json += diceSets[i].toJSON();
+            if (i != diceSets.length - 1) {
+                json += ", "
+            }
+        }
+
+        json += "]"
+        return json;
+    }
+    
 }
 
 
 document.addEventListener('DOMContentLoaded', function() {
+  
+    
     board.display();
 
  }, false);
